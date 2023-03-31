71a2d25908921575cd808a2344e78625dd3d4c68
wissalChebb <98927814+wissalChebb@users.noreply.github.com>
2023-03-31 17:02:36 +0100

    rating

--- lib/components/api_routes.dart
+++ lib/components/api_routes.dart
@@ -1,5 +1,5 @@
 class Api_Routes {
-  static final String base = "http://172.16.6.170:9090/";
+  static final String base = "http://192.168.1.2:9090/";
   static final String products_route = base + 'Produit/';
   static final String payment_route = base + 'payment/';
 
--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:pim/components/default_button.dart';
 import 'package:pim/components/global_repos.dart';
 import 'package:pim/models/Product.dart';
@@ -56,6 +57,37 @@ class Body extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class Rating extends StatefulWidget {
+  const Rating({super.key});
+
+  @override
+  State<Rating> createState() => _RatingState();
+}
+
+class _RatingState extends State<Rating> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: RatingBar.builder(
+        initialRating: 3,
+        minRating: 1,
+        direction: Axis.horizontal,
+        allowHalfRating: true,
+        itemCount: 5,
+        itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+        itemBuilder: (context, _) => Icon(
+          Icons.star,
+          color: Colors.amber,
+        ),
+        onRatingUpdate: (rating) {
+          print(rating);
+        },
+      ),
     );
   }
 }
